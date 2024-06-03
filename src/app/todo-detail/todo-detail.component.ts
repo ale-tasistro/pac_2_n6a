474bc41bb951cad9309baaa0169d5c8851f39b0b
Ale <ale.tasistro@gmail.com>
2024-06-03 19:39:49 -0300

    Added todo detail

--- src/app/todo-detail/todo-detail.component.ts
+++ src/app/todo-detail/todo-detail.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../types';
+import { ActivatedRoute } from '@angular/router';
+import { TodosService } from '../todos.service';
 
 @Component({
   selector: 'app-todo-detail',
   templateUrl: './todo-detail.component.html',
   styleUrls: ['./todo-detail.component.scss']
 })
-export class TodoDetailComponent {
+export class TodoDetailComponent implements OnInit {
+  todo: Todo | null = null;
 
+  constructor(private route: ActivatedRoute, private todosService: TodosService) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.todosService.show(+id).subscribe((data: Todo) => {
+        this.todo = data;
+      });
+    } else {
+      console.error('Missing id');
+    }
+  }
 }
