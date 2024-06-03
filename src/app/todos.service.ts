d34273971942fd4196008220f1967a0fff0b2de4
Ale <ale.tasistro@gmail.com>
2024-06-03 19:02:50 -0300

    Added index method to todo service

--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -1,9 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Todo } from './types';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodosService {
   constructor(private http: HttpClient) {}
+
+  index(): Observable<Todo[]> {
+    const indexEnpodint = 'https://jsonplaceholder.typicode.com/todos';
+    return this.http.get<Todo[]>(indexEnpodint);
+  }
 }
